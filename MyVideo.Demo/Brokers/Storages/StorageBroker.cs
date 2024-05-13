028c1654770514b9480db6288c54270741d69eda
A'zamjon Alijonov <alijonovrobo@gmail.com>
2024-05-13 21:48:44 +0500

    add

--- MyVideo.Demo/Brokers/Storages/StorageBroker.cs
+++ MyVideo.Demo/Brokers/Storages/StorageBroker.cs
@@ -1,6 +1,17 @@
 ﻿namespace MyVideo.Demo.Brokers.Storages
 {
-    internal partial class StorageBroker
+    internal partial class StorageBroker : EFxceptionsContext
     {
+        private readonly IConfiguration configuration;
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate(); 
+        }
+
+        protected override void OnConfiguring(DBContextOptionsBuilder optionsBuilder) 
+        {
+            string connectionString = this.configuration.GetConnectionString();
+        }
     }
 }
